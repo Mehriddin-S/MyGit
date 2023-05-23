c1461558bb061d64a61fe24097ca0eca840a3f70
ali_tech <saidahrorovm@gmail.com>
2023-05-23 13:49:49 +0500

    git2

--- app/src/main/java/uz/gita/mygithub/MainActivity.kt
+++ app/src/main/java/uz/gita/mygithub/MainActivity.kt
@@ -3,11 +3,16 @@ package uz.gita.mygithub
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("kkk", "onCreate: ")
+    }
+
+    fun simple(){
+        Toast.makeText(this, "Nima gap", Toast.LENGTH_SHORT).show()
     }
 }
